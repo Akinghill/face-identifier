0a435acfb8f7ac48ff98b6777c41b408b3d277b6
Austin Hill <austinkinghill@gmail.com>
2020-05-10 20:06:15 -0700

    api recognition functionality

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -8,7 +8,7 @@ export const Logo = () => {
     <div className="Logo ma4 mt0">
       <Tilt className="Tilt br2 shadow2" options={{ max: 55 }} style={{ height: 250, width: 250 }} >
         <div className="Tilt-inner">
-          <img src={anon} alt="site logo"></img>
+          <img className="logo" src={anon} alt="site logo"></img>
         </div>
       </Tilt>
     </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navigation = () => {
   return (
     <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+      <p style={{ color: 'white' }} className="f3 link dim black underline pa3 pointer">Sign Out</p>
     </nav>
   )
 }
