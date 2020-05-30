adcab22cbf2558d05f9f8e6fa244385117732d28
Austin Hill <austinkinghill@gmail.com>
2020-05-30 11:19:03 -0700

    Updates

---
+++ precache-manifest.190ef19700c74cc6d8eb192b6da58b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32111a69b9f296385c34032ff7cd6fa",
+    "url": "/face-identifier/index.html"
+  },
+  {
+    "revision": "b19b326fe7d4044448e3",
+    "url": "/face-identifier/static/css/2.85cdb6f3.chunk.css"
+  },
+  {
+    "revision": "d629352a11774711631b",
+    "url": "/face-identifier/static/css/main.6f4debd9.chunk.css"
+  },
+  {
+    "revision": "b19b326fe7d4044448e3",
+    "url": "/face-identifier/static/js/2.b391b46e.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-identifier/static/js/2.b391b46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d629352a11774711631b",
+    "url": "/face-identifier/static/js/main.6ee0233a.chunk.js"
+  },
+  {
+    "revision": "3ca1bf157c9d9dda98b5",
+    "url": "/face-identifier/static/js/runtime-main.aeacf2d6.js"
+  }
+]);
